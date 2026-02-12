c5efd8b7c2743d98b909869e04fa22214a3d2204
saeed masoudi (Eric) <60331793+saeedmasoudie@users.noreply.github.com>
2026-02-13 00:19:13 +0330

    v1.3.4
    v1.3.4

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ if platform.system() == "Darwin":
             current_path += os.pathsep + p
     os.environ["PATH"] = current_path
 
-CURRENT_VERSION = "1.3.3"
+CURRENT_VERSION = "1.3.4"
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/saeedmasoudie/pywarp/main/version.json"
 WARP_ASSETS = f"https://github.com/saeedmasoudie/pywarp/releases/download/v{CURRENT_VERSION}/warp_assets.zip"
 SERVER_NAME = "PyWarpInstance"
